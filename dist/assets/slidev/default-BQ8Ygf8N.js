34fb2f957c267e9b86fb144183ffae85e0843730
David <david.gozlan@epitech.eu>
2025-05-06 22:04:58 +0200

    test

---
+++ dist/assets/slidev/default-BQ8Ygf8N.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-m23lEbID.js";import{_ as r}from"../index-D-3sg852.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
